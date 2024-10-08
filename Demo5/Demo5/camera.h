8db3719f17d4690c853767f38b495bc8e4e215b3
Zongdi Qiu <690160142@qq.com>
2024-10-08 21:50:41 +0800

    finshi bullet class

--- Demo5/Demo5/camera.h
+++ Demo5/Demo5/camera.h
@@ -1,10 +1,19 @@
 #pragma once
 
 #include "vector2.h"
+#include "timer.h"
 class Camera
 {
 public:
-	Camera() = default;
+	Camera()
+	{
+		timer_shake.set_one_shot(true);
+		timer_shake.set_callback([&]()
+			{
+				is_shaking = false;
+				reset();
+			});
+	}
 	~Camera() = default;
 
 	const Vector2& get_position() const {
@@ -17,10 +26,29 @@ public:
 	}
 
 	void on_update(int delta) {
+
+		timer_shake.on_update(delta);
+
+		if (is_shaking)
+		{
+			position.x = (-50 + rand() % 100) / 50.0f * shaking_strength;
+			position.y = (-50 + rand() % 100) / 50.0f * shaking_strength;
+		}
 		const Vector2 speed = { -0.35f, 0 };
 		position += speed * (float)delta;
 	}
+	void shake(float strength, int duration) 
+	{
+		is_shaking = true;
+		shaking_strength = strength;
+
+		timer_shake.set_wait_time(duration);
+		timer_shake.restart();
+	}
 private:
 	Vector2 position;
+	Timer timer_shake;
+	bool is_shaking = false;
+	float shaking_strength = 0;
 };
 
--- Demo5/Demo5/sun_bullet.h
+++ Demo5/Demo5/sun_bullet.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#include "bullet.h"
+#include "animation.h"
+
+extern Atlas atlas_sun;
+extern Atlas atlas_sun_explode;
+
+class SunBullet : public Bullet
+{
+public:
+	SunBullet() {
+		size.x = 96, size.y = 96;
+
+		damage = 20;
+
+		animation_idle.set_atlas(&atlas_sun);
+		animation_idle.set_interval(50);
+		animation_explode.set_loop(false);
+		animation_explode.set_callback([&]() {can_remove = true; });
+
+		IMAGE* frame_idle = animation_idle.get_frame();
+		IMAGE* frame_explode = animation_explode.get_frame();
+	}
+	~SunBullet() = default;
+
+private:
+	Animation animation_idle;
+	Animation animation_explode;
+	Vector2 explode_render_offset;
+};
